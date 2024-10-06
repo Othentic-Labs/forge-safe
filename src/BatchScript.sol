8d5c063192c0c07229c595ea8f50d6b7ea9a6a96
ronerlih <ronerlih@Rons-MacBook-Pro.local>
2024-10-06 09:57:40 +0300

    feat(BatchScript): Add support for base mainnet

--- src/BatchScript.sol
+++ src/BatchScript.sol
@@ -120,6 +120,9 @@ abstract contract BatchScript is Script {
         } else if (chainId == 84532) {
             SAFE_API_BASE_URL = "https://safe-transaction-base-sepolia.safe.global/api/v1/safes/";
             SAFE_MULTISEND_ADDRESS = 0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761;
+         } else if (chainId == 8453) {
+            SAFE_API_BASE_URL = "https://safe-transaction-base.safe.global/api/v1/safes/";
+            SAFE_MULTISEND_ADDRESS = 0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761;
         } else {
             revert("Unsupported chain");
         }
