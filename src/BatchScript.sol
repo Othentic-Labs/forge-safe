e0b43d2cad0e003c1415253531742af756d326fb
ronerlih <rone@othentic.xyz>
2025-04-29 13:56:25 +0300

    feat: bn safe address and api config

--- src/BatchScript.sol
+++ src/BatchScript.sol
@@ -138,6 +138,9 @@ abstract contract BatchScript is Script, SetChains{
          } else if (chainId == 169) {
             SAFE_API_BASE_URL = "https://gateway.safe.manta.network/v1/chains/169/safes/";
             SAFE_MULTISEND_ADDRESS = 0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761;
+         } else if (chainId == 56) {
+            SAFE_API_BASE_URL = "https://safe-transaction-bsc.safe.global/api/v1/safes/";
+            SAFE_MULTISEND_ADDRESS = 0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761;
          } else {
             revert("Unsupported chain");
         }
